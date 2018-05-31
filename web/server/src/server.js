6896e811c4096dbe8484e7e29c2b4990c1b53ffa
Martin Waltman <martin@mkwiseman.com>
2018-05-31 13:30:26 -0700

    Added web server and client

---
+++ web/server/src/server.js
@@ -0,0 +1,64 @@
+'use strict';
+
+let express = require('express');
+let bodyParser = require('body-parser');
+
+let env = require('../../config/env.json');
+
+const app = express();
+let ready = false;
+
+app.use(bodyParser.json());
+app.use(express.static('client'));
+
+app.listen(env.server.port, () => {
+    ready = true;
+    console.log('Listening on port ' + env.server.port);
+});
+
+app.all('*', (req, res, next) => {
+    if (!ready) {
+        return res.status(503).json({success: false, message: 'Server is not ready yet', data: {}});
+    }
+    next();
+});
+
+app.get('/', (req, res) => {
+    let options = {
+        root: 'client/src/',
+        headers: {
+            'contentType': 'text/html'
+        }
+    };
+
+    res.sendFile('index.html', options, (err) => {
+        if (err) {
+            let msg = 'Could not find index.html';
+            console.error(msg, err);
+            return res.status(404).json({success: false, message: msg, data: {}}).end();
+        }
+    })
+});
+
+app.get('*.js', (req, res) => {
+    let options = {
+        root: 'client/',
+        headers: {
+            'contentType': 'text/javascript'
+        }
+    };
+
+    res.sendFile(req.path, options, (err) => {
+        if (err) {
+            let msg = 'Could not find ' + req.path;
+            console.error(msg, err);
+            return res.status(404).json({success: false, message: msg, data: {}}).end();
+        }
+    })
+});
+
+// TODO: Create a favicon
+app.get('favicon.ico', (req, res) => {
+    console.log('favicon not implemented');
+    return res.status(501).end('Favicon not implemented');
+});
